46628f2efc99cbd786af4c6352e7456ecec33a7c
Marko Kermiciev <kermicievmarko@gmail.com>
2023-01-17 08:48:42 +0100

    ESlint errors fixed

--- modules/script.js
+++ modules/script.js
@@ -1,15 +1,15 @@
-import Books from "./book.js";
-import { DateTime } from "./luxon/src/luxon.js";
+import Books from './book.js';
+import { DateTime } from './luxon/src/luxon.js';
 
 const allBooks = new Books([]);
 
 const addToLocalStorage = () => {
   const stringifyArray = JSON.stringify(allBooks.bookArray);
-  localStorage.setItem("storedBooks", stringifyArray);
+  localStorage.setItem('storedBooks', stringifyArray);
 };
 
 // Clock function
-const clockElement = document.getElementById("date");
+const clockElement = document.getElementById('date');
 const clock = () => {
   clockElement.textContent = DateTime.now().toISO();
 };
@@ -17,25 +17,25 @@ setInterval(clock, 1000);
 
 // List of books
 const displayBook = () => {
-  const addedBooks = document.getElementById("list");
-  addedBooks.innerHTML = "";
+  const addedBooks = document.getElementById('list');
+  addedBooks.innerHTML = '';
   for (let i = 0; i < allBooks.bookArray.length; i += 1) {
-    const container = document.createElement("div");
-    container.classList.add("book");
+    const container = document.createElement('div');
+    container.classList.add('book');
     addedBooks.appendChild(container);
 
-    const container02 = document.createElement("div");
-    container02.classList.add("titleAuthor");
+    const container02 = document.createElement('div');
+    container02.classList.add('titleAuthor');
     container.appendChild(container02);
 
-    const bookDetails = document.createElement("p");
-    bookDetails.classList.add("title");
+    const bookDetails = document.createElement('p');
+    bookDetails.classList.add('title');
     bookDetails.textContent = `"${allBooks.bookArray[i].title}" by ${allBooks.bookArray[i].author}`;
     container02.appendChild(bookDetails);
 
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove");
-    removeButton.textContent = "Remove";
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove');
+    removeButton.textContent = 'Remove';
 
     removeButton.onclick = () => {
       allBooks.booksFilter(allBooks.bookArray[i]);
@@ -47,50 +47,50 @@ const displayBook = () => {
 };
 
 const getFromLocalStorage = () => {
-  const stringifyArray = localStorage.getItem("storedBooks");
+  const stringifyArray = localStorage.getItem('storedBooks');
   allBooks.bookArray = JSON.parse(stringifyArray);
   displayBook();
 };
 
-if (localStorage.getItem("storedBooks") == null) {
+if (localStorage.getItem('storedBooks') == null) {
   addToLocalStorage();
 } else {
   getFromLocalStorage();
 }
 
-const addBtn = document.getElementById("addButton");
-addBtn.addEventListener("click", () => {
-  const title = document.getElementById("title");
-  const author = document.getElementById("author");
+const addBtn = document.getElementById('addButton');
+addBtn.addEventListener('click', () => {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
   allBooks.bookObj(title.value, author.value);
   addToLocalStorage();
   displayBook();
 });
 
 // Page contents
-const list = document.getElementById("list");
-const addNew = document.getElementById("addNew");
-const contactSection = document.getElementById("contact-section");
+const list = document.getElementById('list');
+const addNew = document.getElementById('addNew');
+const contactSection = document.getElementById('contact-section');
 
 // Nav buttons.
-const listLink = document.getElementById("list-link");
-const addLink = document.getElementById("add-link");
-const contactLink = document.getElementById("contact-link");
+const listLink = document.getElementById('list-link');
+const addLink = document.getElementById('add-link');
+const contactLink = document.getElementById('contact-link');
 
-listLink.addEventListener("click", () => {
-  list.classList.remove("hide");
-  addNew.classList.add("hide");
-  contactSection.classList.add("hide");
+listLink.addEventListener('click', () => {
+  list.classList.remove('hide');
+  addNew.classList.add('hide');
+  contactSection.classList.add('hide');
 });
 
-addLink.addEventListener("click", () => {
-  addNew.classList.remove("hide");
-  list.classList.add("hide");
-  contactSection.classList.add("hide");
+addLink.addEventListener('click', () => {
+  addNew.classList.remove('hide');
+  list.classList.add('hide');
+  contactSection.classList.add('hide');
 });
 
-contactLink.addEventListener("click", () => {
-  contactSection.classList.remove("hide");
-  addNew.classList.add("hide");
-  list.classList.add("hide");
+contactLink.addEventListener('click', () => {
+  contactSection.classList.remove('hide');
+  addNew.classList.add('hide');
+  list.classList.add('hide');
 });
